7bd2d7461738bacc3800c51e82a496581405c65c
SchlechtePoesieIstProgramm <s.j.leu@freenet.de>
2022-12-14 18:35:37 +0100

    Dokumentation im Code

--- Chatbot/actions/actions.py
+++ Chatbot/actions/actions.py
@@ -13,12 +13,16 @@ class ActionArtist(Action):
         return "action_get_similar_artist"
 
     def run(self, dispatcher, tracker, domain):
+        #search ist die Variable "search", die vom Bot übergeben wird; sie repräsentiert das, was der user suchen will
         search = tracker.get_slot("search")
         if str(search).strip() == None:
-            dispatcher.utter_message("Du hast doch noch garnichts eingegeben")
+            #Antwort, wenn nichts eingegeben wurde
+            dispatcher.utter_message("Du hast doch noch garnichts eingegeben") #Fehlermeldung, wenn nichts eingegeben wurde
         else:
             result1, result2, result3, result4, result5, result6 = spotify.getRelatedArtist(search)
 
+            #--------------------------------------------
+            #Antwort bei erfolgreicher Anfrage
             answer = f"Ich hätte die hier für dich gefunden: {result1}"
             if result2 != "":
                 answer = f"Ich hätte die hier für dich gefunden: {result1}, {result2}"
@@ -30,12 +34,16 @@ class ActionArtist(Action):
                 answer = f"Ich hätte die hier für dich gefunden: {result1}, {result2}, {result3}, {result4}, {result5}"
             if result6 != "":
                 answer = f"Ich hätte die hier für dich gefunden: {result1}, {result2}, {result3}, {result4}, {result5}, {result6}"
-            
+            #--------------------------------------------
+
             if "error" in result1:
-                dispatcher.utter_message("Sorry! Das habe ich nicht erkannt!")
+                #Fehlermeldung, wenn Artist nicht existiert
+                dispatcher.utter_message("Den Artist kenn' ja nicht mal ich! Gib mal was anderes ein") 
             elif "No similar" in result1:
-                dispatcher.utter_message("Ich kenne leider nichts ähnliches dazu")
+                #Fehlermeldung, wenn es nichts Ähnliches zur Suche gibt
+                dispatcher.utter_message("Ich kenne leider nichts ähnliches dazu") 
             else:
+                #Ausgabe der Antwort, wenn die Anfrage erfolgreich war
                 dispatcher.utter_message(answer)
 
         return []
@@ -46,10 +54,13 @@ class ActionTrack(Action):
         return "action_get_similar_track"
 
     def run(self, dispatcher, tracker, domain):
+        #search ist die Variable "search", die vom Bot übergeben wird; sie repräsentiert das, was der user suchen will
         search = tracker.get_slot("search")
         if str(search).strip() == None:
+            #Antwort, wenn nichts eingegeben wurde
             dispatcher.utter_message("Du hast doch noch garnichts eingegeben")
         else:
+            #Ausgabe der Antwort, wenn die Anfrage erfolgreich war
             dispatcher.utter_message("Hier gibt es noch nichts zu sehen")
 
         return []
