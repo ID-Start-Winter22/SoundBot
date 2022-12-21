7e4dfebbc260f52f5618207fbd22c8104c4b687e
SchlechtePoesieIstProgramm <s.j.leu@freenet.de>
2022-12-21 17:56:44 +0100

    Change directory name

--- Chatbot/start_rasa.sh
+++
@@ -1,3 +0,0 @@
-# rasa run --cors "*" --enable-api & rasa run actions &
-screen -S rasa -d -m rasa run --cors "*" --enable-api
-screen -S rasa-actions -d -m rasa run actions
--- Chatbot/stop_rasa.sh
+++
@@ -1,2 +0,0 @@
-screen -X -S rasa quit
-screen -X -S rasa-actions quit
