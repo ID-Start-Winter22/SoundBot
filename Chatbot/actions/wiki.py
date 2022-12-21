933ebe2088f6d182cca9fe562692b7b088eea3ee
SchlechtePoesieIstProgramm <s.j.leu@freenet.de>
2022-12-21 17:55:08 +0100

    #22

--- Chatbot/actions/wiki.py
+++ Chatbot/actions/wiki.py
@@ -1,12 +1,16 @@
 import requests
+#import markdown
 import json
 
 def getInfo(name: str):
-    url = f"https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles={name}"
+    url = f"https://de.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles={name}"
     res = requests.get(url=url).content
     json_data = json.loads(res)
     page = json_data["query"]["pages"]
-    info = page
+    pageId = list(dict(page).keys())[0]
+    info = page[str(pageId)]["extract"]
+    info = str(info)[:str(info).find("</p>")].strip("<p>")
+    #info = markdown.markdown(info, heading_style="ATX")
     return info
 
-#print(getInfo("Eminem"))
+print(getInfo("Michael Jackson"))
