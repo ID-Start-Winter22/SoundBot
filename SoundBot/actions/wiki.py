1f8655e44128e5c9c02b43fb9e7f0d3e579244db
SchlechtePoesieIstProgramm <s.j.leu@freenet.de>
2023-01-20 07:48:46 +0100

    Anpassen der Ausgabe im Chat

--- SoundBot/actions/wiki.py
+++ SoundBot/actions/wiki.py
@@ -1,6 +1,12 @@
 import requests
-import markdown
+import markdownify
 import json
+
+def joinParts(parts: list) -> str:
+    info = ""
+    for i in range(len(parts)):
+        info = info + parts[i]
+    return info
 
 def getInfo(name: str):
     """gibt Infos zu einem gegebenen Namen aus"""
@@ -13,10 +19,16 @@ def getInfo(name: str):
     page = json_data["query"]["pages"]
     pageId = list(dict(page).keys())[0]
     info = page[str(pageId)]["extract"]
-    info = str(info)[:str(info).find("</p>")].strip("<p>")
+    info = str(info)[:str(info).find("</p>")]
 
     #hier müsste der Text von HTML zu Markdown umgewandelt werden; Ich weiß noch nicht wie
-    info = markdown.markdown(info, heading_style="ATX")
+    info = markdownify.markdownify(info)
+
+    infoParts = info.split("**")
+    info = joinParts(infoParts)
+    infoParts = info.split("\n")
+    info = joinParts(infoParts)
+
     return info
 
 print(getInfo("Michael Jackson"))
